dfda4100ed551696e6b0b5b20960f958582e1863
Sparsh Johri <bhatabhaiyya@gmail.com>
2018-12-25 17:04:03 -0800

    The previous version had a mistake.

--- bump_sensor.py
+++ bump_sensor.py
@@ -9,19 +9,6 @@ Motor 		= vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO18_1, False)
 bumper_a    = vex.Bumper(brain.three_wire_port.a)
 #endregion config
 
-# Carefully now...
-
-while True:
-  # Creep back...
-  dt.drive(vex.DirectionType.REV)
-  sys.wait_for(bumper_a.pressing)
-  # Ran into something, move away from it
-  dt.drive(vex.DirectionType.FWD)
-  sys.sleep(2)
-  # Turn a bit to avoid it...
-  dt.turn_for(vex.TurnType.LEFT, 77, vex.RotationUnits.DEG)
-  # Ready to try again!
-
 
 def autonomous():
 	while True:
