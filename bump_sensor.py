f19232a41d96995652f99cebc30a56d165fba430
Anirudh <anirudhpalaiya@gmail.com>
2018-12-25 17:39:27 -0800

    fixed bumper code

--- bump_sensor.py
+++ bump_sensor.py
@@ -1,23 +1,30 @@
 # VEX V5 Python Project
-import sys
+# import sys
 import vex
 from vex import *
 
-#region config
-brain       = vex.Brain();
-Motor 		= vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO18_1, False)
-bumper_a    = vex.Bumper(brain.three_wire_port.a)
-#endregion config
+# region config
+brain = vex.Brain();
+Motor = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO18_1, False)
+bumper_b = vex.Bumper(brain.three_wire_port.b)
+
+
+# endregion config
 
 
 def autonomous():
-	while True:
-		if bumper_a.pressing()==False:
-			Motor.spin(vex.DirectionType.FWD, 40, vex.VelocityUnits.PCT)
-	pass	
+    while True:
+        print(bumper_b.pressing())
+        if bumper_b.pressing() == 0:
+            Motor.spin(vex.DirectionType.FWD, 40, vex.VelocityUnits.PCT)
+
+        else:
+            Motor.spin(vex.DirectionType.FWD, 0, vex.VelocityUnits.PCT)
+        sys.sleep(1)
+
 
 def drivercontrol():
-	pass
+    pass
 
 
 competition = vex.Competition()
