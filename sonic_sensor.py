df10dcdc5055aa116fbaa8223b4f535c049de0df
Anirudh <anirudhpalaiya@gmail.com>
2018-12-25 18:24:24 -0800

    Completed ultrasonic sensor program Motor stops when within 10 in.

--- sonic_sensor.py
+++ sonic_sensor.py
@@ -1,25 +1,47 @@
-# VEX V5 Python-Project with Competition Template
-#Imports
+# VEX V5 Python Project
 import sys
 import vex
-import math
+from vex import *
 
-
-
-
-#Variables
-brain       = vex.Brain()
-dev         = vex.Devices()
-con         = vex.Controller(vex.ControllerType.PRIMARY)
-
-#output cable connected to port C and the inpur connected to port d
-#output and input have to be on the consecutive ports in that order
-#Sonar has two three wire connection - one is termed as input and the other
-#as the output
+# region config
+brain = vex.Brain();
+Motor = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO18_1, False)
+# dev         = vex.Devices()
+# con         = vex.Controller(vex.ControllerType.PRIMARY)
 sonar = vex.Sonar(brain.three_wire_port.c)
 
-while True :
-    print sonar.distance(vex.DistanceUnits.MM)
-    sys.sleep(1)
+
+# endregion config
+
+
+def autonomous():
+    while True:
+        sonardistance = sonar.distance(DistanceUnits.IN)
+        print(sonardistance)
+        if sonardistance < 10:
+            Motor.spin(vex.DirectionType.FWD, 0, vex.VelocityUnits.PCT)
+        else:
+            Motor.spin(vex.DirectionType.FWD, 40, vex.VelocityUnits.PCT)
+
+        # if bumper_b.pressing() == 0:
+        #     Motor.spin(vex.DirectionType.FWD, 40, vex.VelocityUnits.PCT)
+        #
+        # else:
+        #     Motor.spin(vex.DirectionType.FWD, 0, vex.VelocityUnits.PCT)
+        sys.sleep(1)
+
+
+def drivercontrol():
+    pass
+
+
+competition = vex.Competition()
+competition.autonomous(autonomous)
+competition.drivercontrol(drivercontrol)
+
+# output cable connected to port C and the inpur connected to port d
+# output and input have to be on the consecutive ports in that order
+# Sonar has two three wire connection - one is termed as input and the other
+# as the output
 
 print("*****end******")
