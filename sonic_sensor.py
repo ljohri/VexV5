17cddab46886afa7e4b69167f7f0572f55ef6ebd
Sparsh Johri <bhatabhaiyya@gmail.com>
2018-12-29 10:29:45 -0800

    This happened to pop up in my unstaged files, for some reason.

--- sonic_sensor.py
+++ sonic_sensor.py
@@ -1,12 +1,13 @@
 # VEX V5 Python-Project with Competition Template
 #Imports
+print("Checkpoint 0")
 import sys
 import vex
 import math
 
 
 
-
+print("Checkpoint 1")
 #Variables
 brain       = vex.Brain()
 dev         = vex.Devices()
@@ -17,9 +18,22 @@ con         = vex.Controller(vex.ControllerType.PRIMARY)
 #Sonar has two three wire connection - one is termed as input and the other
 #as the output
 sonar = vex.Sonar(brain.three_wire_port.c)
+print("Checkpoint 2")
+def autonomous():
+    print("Checkpoint 3")
+	while True :
+	    print ( sonar.distance(vex.DistanceUnits.CM) )
 
-while True :
-    print sonar.distance(vex.DistanceUnits.MM)
-    sys.sleep(1)
+	print("Checkpoint 4")
+	
+
+def drivercontrol():
+    print("Checkpoint 6")
+
+print("Checkpoint 7")
+competition = vex.Competition()
+competition.autonomous(autonomous)
+competition.drivercontrol(drivercontrol)
+print("Checkpoint 8")
 
 print("*****end******")
